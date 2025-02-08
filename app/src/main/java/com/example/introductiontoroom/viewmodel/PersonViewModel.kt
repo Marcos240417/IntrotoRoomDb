2ef436e43af7c024156abcd608dd9452e56e2dcf
Eriberto Junior <eriberto.brasileiro@gmail.com>
2025-02-08 19:44:37 -0300

    Definindo dispatcher no construtor da ViewModel, evitando replicação de código

--- app/src/main/java/com/example/introductiontoroom/viewmodel/PersonViewModel.kt
+++ app/src/main/java/com/example/introductiontoroom/viewmodel/PersonViewModel.kt
@@ -7,11 +7,15 @@ import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.introductiontoroom.data.PersonRepository
 import com.example.introductiontoroom.data.model.PersonEntity
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
-class PersonViewModel(private val personRepository: PersonRepository) : ViewModel() {
+class PersonViewModel(
+    private val personRepository: PersonRepository,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) : ViewModel() {
 
     // Convertendo Flow para LiveData
     val allPersons: LiveData<List<PersonEntity>> = personRepository.getAllPerson().asLiveData()
@@ -20,35 +24,35 @@ class PersonViewModel(private val personRepository: PersonRepository) : ViewMode
 
     // Função para adicionar uma pessoa
     fun addPerson(personEntity: PersonEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(dispatcher) {
             personRepository.insertPerson(personEntity)
         }
     }
 
     // Função para excluir uma pessoa
     fun deletePerson(personEntity: PersonEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(dispatcher) {
             personRepository.deletePerson(personEntity)
         }
     }
 
     // Função para atualizar os dados de uma pessoa
     fun updatePerson(personEntity: PersonEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(dispatcher) {
             personRepository.updatePerson(personEntity)
         }
     }
 
     // Função para excluir uma pessoa pelo ID
     fun deletePersonById(personEntity: PersonEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(dispatcher) {
             personRepository.deletePersonById(personEntity)
         }
     }
 
     // Função para procurar uma pessoa com base no nome, idade ou cidade
     fun getSearchedData(query: String) {
-        viewModelScope.launch(Dispatchers.IO) {
+        viewModelScope.launch(dispatcher) {
             val result: Flow<List<PersonEntity>> = personRepository.getSearchedData(query)
             result.collect { personList ->
                 _searchedPersons.postValue(personList)
